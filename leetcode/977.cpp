89b28340c2f5c25cfc4fd9aa6dbde6aafb02646d
Json Lee <lijiansong4@huawei.com>
2021-07-30 00:34:25 +0800

    automatically

---
+++ leetcode/977.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> sortedSquares(vector<int> &nums) {
+    vector<int> ans(nums.begin(), nums.end());
+  }
+};
