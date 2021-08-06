393c1f62f7c6af13935dbe72f751586e01c8d7a0
Json Lee <87839054+jsonlee0x01@users.noreply.github.com>
2021-08-06 09:41:18 +0800

    automatically

---
+++ leetcode/58.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s)
+    {
+        int count = 0;
+        reverse(s.begin(), s.end());
+        if (s[0] == ' ') {
+            // 从第一个不是空格的地方开始count
+            int i = 1;
+            while (s[i] == ' ') {
+                ++i;
+            }
+            while (i < s.size() && s[i] != ' ') {
+                ++i;
+                ++count;
+            }
+        } else {
+            int i = 0;
+            while (i < s.size() && s[i] != ' ') {
+                ++count;
+                ++i;
+            }
+        }
+        return count;
+    }
+};
