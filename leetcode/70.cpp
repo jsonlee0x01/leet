2ef04a81d111c42acd8bd34ac8b05dc8f15b249d
Json Lee <lijiansong4@huawei.com>
2021-08-08 22:39:51 +0800

    automatically

---
+++ leetcode/70.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int climbStairs(int n) {
+    if (n <= 2) {
+      return n;
+    }
+    vector<int> f(n + 1);
+    f[0] = 0;
+    f[1] = 1;
+    f[2] = 2;
+    for (int i = 3; i < n + 1; ++i) {
+      f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+  }
+};
