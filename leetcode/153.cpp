183d62e79fdfe70e7922eaaef331f31671d3dfd6
Json Lee <lijiansong4@huawei.com>
2021-08-09 23:01:06 +0800

    automatically

---
+++ leetcode/153.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int findMin(vector<int> &nums) {
+    int l = 0;
+    int r = nums.size() - 1;
+    while (l < r) {
+      int mid = (r - l) / 2 + l;
+      if (nums[mid] < nums[r]) {
+        r = mid;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return nums[l];
+  }
+};
