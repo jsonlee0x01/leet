74f0f8dc767af422985e8c6fcbd8aeb7d43286a9
Json Lee <lijiansong4@huawei.com>
2021-08-22 00:57:18 +0800

    automatically

--- leetcode/621.cpp
+++ leetcode/621.cpp
@@ -4,5 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-  int leastInterval(vector<char> &tasks, int n) {}
+  int leastInterval(vector<char> &tasks, int n) {
+    vector<int> count(26, 0);
+    for (int i = 0; i < tasks.size(); ++i) {
+      ++count[tasks[i] - 'A'];
+    }
+    sort(count.begin(), count.end());
+    int maxCount = 0;
+    for (int i = 25; i >= 0; --i) {
+      if (count[i] == count[25]) {
+        ++maxCount;
+      } else {
+        break;
+      }
+    }
+    return max((size_t)(count[25] - 1) * (n + 1) + maxCount, tasks.size());
+  }
 };
