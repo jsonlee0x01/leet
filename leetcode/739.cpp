3389d6b9f1fc4cc6a766399edbe43dea2dac8b3e
Json Lee <87839054+jsonlee0x01@users.noreply.github.com>
2021-08-28 10:40:42 +0800

    automatically

--- leetcode/739.cpp
+++ leetcode/739.cpp
@@ -1,4 +1,40 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+// 单调栈，维护单调递增栈，每次pop的时候记录相关信息
 class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        vector<int> ans(temperatures.size(), 0);
+        stack<pair<int, int>> incStack;
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!incStack.empty() && temperatures[i] > incStack.top().first) {
+                ans[incStack.top().second] = i - incStack.top().second;
+                incStack.pop();
+            }
+            incStack.push({temperatures[i], i});
+        }
+        return ans;
+    }
+
+    vector<int> dailyTemperatures1(vector<int> &temperatures)
+    {
+        vector<int> ans(temperatures.size(), 0);
+        stack<int> incStack;
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!incStack.empty() && temperatures[i] > temperatures[incStack.top()]) {
+                ans[incStack.top()] = i - incStack.top();
+                incStack.pop();
+            }
+            incStack.push(i);
+        }
+        return ans;
+    }
+};
+
+class Solution1 {
 public:
     vector<int> dailyTemperatures(vector<int> &temperatures)
     {
